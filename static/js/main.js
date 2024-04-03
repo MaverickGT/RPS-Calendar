0095b7512523c3b68e0bdf8fd4c835becccbf9e3
georgi-terziyski <georgi.terziyski@hpe.com>
2024-04-03 22:42:35 +0300

    added main.js

---
+++ static/js/main.js
@@ -0,0 +1,34 @@
+function getValue() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("feedback_text").value;
+
+  var data = {
+    name: name,
+    email: email,
+    feedback: message,
+  };
+  return data;
+}
+function clearFeedback() {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("feedback_text").value = "";
+}
+
+function submitFeedback() {
+  var data = getValue();
+  console.log(data);
+  fetch("http://localhost:5000/api/feedback", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      clearFeedback();
+    });
+}
