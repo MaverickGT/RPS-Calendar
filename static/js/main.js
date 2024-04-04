cfa7c90a2b1daf6d012c10fc06a316037a321028
georgi-terziyski <georgi.terziyski@hpe.com>
2024-04-04 17:09:38 +0300

    Update margins, font sizes, and padding in CSS*** ***Add loadCalendar function to main.js*** ***Update event types and footer text in index.html

--- static/js/main.js
+++ static/js/main.js
@@ -48,3 +48,14 @@ function getEvents() {
       });
     });
 }
+
+function loadCalendar() {
+  document.addEventListener("DOMContentLoaded", function () {
+    var calendarEl = document.getElementById("calendar");
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: "dayGridMonth",
+    });
+    calendar.render();
+    calendar.updateSize();
+  });
+}
